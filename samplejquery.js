2607c5aedf0e94cecf3d11272df0c4ca5b7bd714
dnuvvula <dnuvvula@hugeinc.com>
2018-04-12 10:41:48 -0400

    removed the jquery definition

--- samplejquery.js
+++ samplejquery.js
@@ -1,16 +1,18 @@
+
 // dummy script for AEM embedding
 
-(function() {
-
+(function($) {
 console && console.log('Embedded dummy script called');
 
 try {
+  if ( $ && $(document).jquery ) {
     $(document).ready(function() {
       console && console.log('Embedded dummy script: jQuery found and ready');
     });
+  }
 }
 catch(e) {
   console && console.error('Embedded dummy script: error testing for jQuery', e);
 }
 
-})();
+})(jQuery);
